fabfbef00c02d40711391b0e31ade0a832b981bb
Efe Polat <mefepolat@hotmail.com>
2023-02-21 15:33:53 -0500

    project and user setup, first commit

---
+++ server/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    employer: {
+        type: Boolean,
+        default: false
+    },
+    employee: {
+        type: Boolean,
+        default: true
+    }
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
---
+++ server/server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const mongoSanitize = require('express-mongo-sanitize');
+const LocalStrategy = require('passport-local');
+const cors = require('cors');
+const MongoStore = require('connect-mongo');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+app.use(
+    cors({
+        origin: "http://localhost:3001",
+        optionsSuccessStatus: 200,
+        credentials: true
+    })
+);
+const dbUrl = "mongodb://127.0.0.1:27017/staffing-ai";
+
+mongoose.set('strictQuery', false);
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => {
+    console.log('database connection is successful');
+});
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret: 'justasecretfornow'
+    },
+    collectionName: 'sessions'
+});
+
+const sessionConfig = {
+    store: store,
+    name: 'session',
+    secret: 'justasecretfornow',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+};
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session(sessionConfig));
+app.use(express.urlencoded({extended: true}));
+app.use(mongoSanitize());
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
