8406d999205a833a7549f15dcdcd1d7bfe39572b
Efe Polat <mefepolat@hotmail.com>
2023-02-22 11:41:35 -0500

    smal fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import  Button  from 'react-bootstrap/Button';
+import "bootstrap/dist/css/bootstrap.min.css.map";
 
 function App() {
   return (
    <div>
     <h1>Staffing App</h1>
+    <Button>button</Button>
    </div>
   );
 }
