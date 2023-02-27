0b57eba99ec11c88517d5a6101212bd34eb19e77
Efe Polat <mefepolat@hotmail.com>
2023-02-27 12:44:05 -0500

    added calendar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import  Button  from 'react-bootstrap/Button';
 import "bootstrap/dist/css/bootstrap.min.css.map";
+import Calendar from './home/components/Calendar';
+
 
 function App() {
   return (
    <div>
     <h1>Staffing App</h1>
     <Button>button</Button>
+    <Calendar />
    </div>
   );
 }
